5249f11274ddd8e422da54ebc1061f0bc32c2515
ibnsan <antvelkol@gmail.com>
2024-09-03 18:34:46 +0200

    Update npm dependencies for @noble/secp256k1, @tanstack/react-query, and dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/main.css";
-import { BlockchainProvider } from "./context/BlockchainProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/main.css';
+import { BlockchainProvider } from './context/BlockchainProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BlockchainProvider>
-      <App />
-    </BlockchainProvider>
+    <QueryClientProvider client={queryClient}>
+      <BlockchainProvider>
+        <App />
+      </BlockchainProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
