8d60d8320fde20f3a3795d7ea335892a710c0062
ibnsan <antvelkol@gmail.com>
2024-09-19 19:23:13 +0200

    Testing toast notification support

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -1,50 +1,46 @@
 import React from 'react';
 import { useBlockchain } from '../hooks/useBlockchain';
 import { useUserStore } from '../store/useUserStore';
+import { useToast } from '../store/useToast';
 
 const ConnectWallet: React.FC = () => {
   const { connectEthereum, connectTron, switchNetwork, provider, tronWeb } = useBlockchain();
   const { address, connectedNetwork } = useUserStore();
+  const { showToast } = useToast();
 
+  function openToast(): void {
+    showToast({ message: 'Wallet connected!' });
+  }
 
   return (
     <div className="flex space-x-4">
       {!provider && !tronWeb && !connectedNetwork ? (
         <>
-          <button
-            onClick={connectEthereum}
-            className="m-2 p-2 bg-blue-400"
-          >
+          <button onClick={connectEthereum} className="m-2 p-2 bg-blue-400">
             Connect to Ethereum
           </button>
-          <button
-            onClick={connectTron}
-            className="m-2 p-2 bg-red-400"
-          >
+          <button onClick={connectTron} className="m-2 p-2 bg-red-400">
             Connect to Tron
           </button>
         </>
       ) : (
         <>
-        <p>Address: {address}</p>
+          <p>Address: {address}</p>
           {provider && (
-            <button
-              onClick={() => switchNetwork('tron')}
-              className="m-2 p-2 bg-red-400"
-            >
+            <button onClick={() => switchNetwork('tron')} className="m-2 p-2 bg-red-400">
               Switch to Tron
             </button>
           )}
           {tronWeb && (
-            <button
-              onClick={() => switchNetwork('ethereum')}
-              className="m-2 p-2 bg-blue-400"
-            >
+            <button onClick={() => switchNetwork('ethereum')} className="m-2 p-2 bg-blue-400">
               Switch to Ethereum
             </button>
           )}
         </>
       )}
+      <button onClick={() => openToast()} className="m-2 p-2 bg-gray-400">
+        Test
+      </button>
     </div>
   );
 };
