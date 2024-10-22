6cc54cf496e78cb431ebacfd2a9d1852c0b9d883
Anton Kolot <antvelkol@gmail.com>
2024-10-22 18:28:32 +0200

    A basic Header component has been created for further development

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Main from "./MainHeader";
+
+/**
+ * @component Header
+ * @description The Header component provides a modular structure for the header section of the layout.
+ */
+const Header = {
+  Main
+};
+
+export default Header;
---
+++ src/components/Header/MainHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @component Main Header Component
+ * @description The Main component for the header section of the layout.
+ */
+const Main: React.FC = () => {
+  return (
+      <div className="w-full flex flex-col">
+
+      </div>
+  );
+};
+
+export default Main;
