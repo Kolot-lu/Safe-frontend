7722ea6ac22221335d9738d695464ff563c39a4c
Anton Kolot <antvelkol@gmail.com>
2024-10-29 18:23:02 +0100

    Added global animation settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        height: 'height',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(0px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeOut: {
+          '0%': { opacity: 1, transform: 'translateY(0)' },
+          '100%': { opacity: 0, transform: 'translateY(0px)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.2s ease-out forwards',
+        fadeOut: 'fadeOut 0.2s ease-out forwards',
+      },
+    },
   },
   plugins: [],
-  darkMode: 'selector'
-}
-
+  darkMode: 'selector',
+};
