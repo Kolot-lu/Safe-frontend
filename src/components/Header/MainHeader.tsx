778716aad3b50b73792c375a02e924580d05dd79
Anton Kolot <antvelkol@gmail.com>
2024-10-30 18:55:44 +0100

    Updated header style

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,42 +1,38 @@
 import React from 'react';
-import { Sun, Moon } from 'lucide-react';
+import { Link } from 'react-router-dom';
 import { mainMenu } from '../../config/menu';
 import Button from '../ui/Button';
-import { useTheme } from '../../hooks/useTheme';
-import SafeLogotype from "../../assets/logos/safe-logotype.svg?react";
-
+import SafeLogotype from '../../assets/logos/safe-logotype.svg?react';
+import LanguageSwitcher from '../LanguageSwitcher';
+import ThemeSwitcher from '../ThemeSwitcher';
 
 /**
  * @component Main Header Component
  * @description The Main component for the header section of the layout. Includes theme toggle functionality.
  */
 const MainHeader: React.FC = () => {
-  const { theme, toggleTheme } = useTheme();
-
   return (
-    <nav className="w-full flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-800">
+    <nav className="w-full flex items-center justify-between py-2 px-3 rounded-md my-3 bg-gray-100 dark:bg-dark-400 border border-border-light dark:border-border-dark">
       <nav>
-        <SafeLogotype className={"w-[85px] h-auto text-black dark:text-white"} />
+        <Link to={'/'}>
+          <SafeLogotype className={'w-[75px] h-auto text-black dark:text-white'} />
+        </Link>
       </nav>
 
       <nav className="flex space-x-4">
         {mainMenu
           .filter((item) => item.isVisible)
           .map((item) => (
-            <Button asLink size="small" key={item.path} to={item.path} className="flex items-center space-x-2">
+            <Button asLink variant="link" size="small" key={item.path} to={item.path}>
               {item.icon && <item.icon />}
               {item.title}
             </Button>
           ))}
       </nav>
 
-      <nav>
-        <button
-          onClick={toggleTheme}
-          className="p-2 rounded-full focus:outline-none bg-gray-200 dark:bg-gray-600"
-        >
-          {theme === 'light' ? <Moon className="w-5 h-5" /> : <Sun className="w-5 h-5" />}
-        </button>
+      <nav className="flex items-center gap-4">
+        <LanguageSwitcher />
+        <ThemeSwitcher />
       </nav>
     </nav>
   );
