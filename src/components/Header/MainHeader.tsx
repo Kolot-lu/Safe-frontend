a31876d147f40914c077f45e06ca82bd19ede262
Anton Kolot <antvelkol@gmail.com>
2024-11-04 18:08:34 +0100

    Add responsive modal menu to MainHeader with wallet connect, language, and theme switcher

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -5,6 +5,9 @@ import Button from '../ui/Button';
 import SafeLogotype from '../../assets/logos/safe-logotype.svg?react';
 import LanguageSwitcher from '../LanguageSwitcher';
 import ThemeSwitcher from '../ThemeSwitcher';
+import WalletConnect from '../WalletConnect';
+import { Menu } from 'lucide-react';
+import Modal from '../ui/Modal/Modal';
 
 /**
  * @component Main Header Component
@@ -12,14 +15,14 @@ import ThemeSwitcher from '../ThemeSwitcher';
  */
 const MainHeader: React.FC = () => {
   return (
-    <nav className="w-full flex items-center justify-between py-2 px-3 rounded-md my-3 bg-gray-100 dark:bg-dark-400 border border-border-light dark:border-border-dark">
+    <nav className="w-full flex items-center justify-between py-2 px-3 rounded-md bg-gray-100 dark:bg-dark-400 border border-border-light dark:border-border-dark">
       <nav>
         <Link to={'/'}>
           <SafeLogotype className={'w-[75px] h-auto text-black dark:text-white'} />
         </Link>
       </nav>
 
-      <nav className="flex space-x-4">
+      <nav className="hidden md:flex space-x-4">
         {mainMenu
           .filter((item) => item.isVisible)
           .map((item) => (
@@ -30,7 +33,33 @@ const MainHeader: React.FC = () => {
           ))}
       </nav>
 
+
       <nav className="flex items-center gap-4">
+      <Modal className="md:hidden">
+        <Modal.Trigger>
+          <Button size="small" rounded>
+            <Menu />
+          </Button>
+        </Modal.Trigger>
+        <Modal.Content className='flex flex-col'>
+ 
+          {mainMenu
+            .filter((item) => item.isVisible)
+            .map((item) => (
+              <Button asLink variant="link" className='justify-start' key={item.path} to={item.path}>
+                {item.icon && <item.icon />}
+                {item.title}
+              </Button>
+            ))}
+
+                    <WalletConnect />
+        <LanguageSwitcher />
+        <ThemeSwitcher />
+        
+        </Modal.Content>
+      </Modal>
+
+        <WalletConnect />
         <LanguageSwitcher />
         <ThemeSwitcher />
       </nav>
