b65c5bde31398acf3b13082173749f62fdfa49ec
Anton Kolot <antvelkol@gmail.com>
2025-01-29 18:45:14 +0100

    Update DropdownContent component to support position customization

--- src/components/ui/Dropdown/DropdownContent.tsx
+++ src/components/ui/Dropdown/DropdownContent.tsx
@@ -6,9 +6,11 @@ import { cn } from '../../../utils/cn';
  * @component DropdownContent
  * @description The dropdown content that appears when triggered, with a fade-in/out animation.
  */
-export const DropdownContent: React.FC<{ closeOnClisk?: boolean; children: ReactNode }> = ({
-  closeOnClisk = true,
+export const DropdownContent: React.FC<{children: ReactNode, position?: "left" | "right", closeOnClisk?: boolean; className?: string }> = ({
   children,
+  position = "right",
+  closeOnClisk = true,
+  className
 }) => {
   const context = useContext(DropdownContext);
   if (!context) throw new Error('DropdownContent must be used within a Dropdown');
@@ -29,8 +31,13 @@ export const DropdownContent: React.FC<{ closeOnClisk?: boolean; children: React
         role="menu"
         aria-labelledby="dropdown-trigger"
         className={cn(
-          'absolute right-0 mt-2 w-fit rounded-md flex flex-col gap-2 p-3 bg-gray-100/30 dark:bg-dark-400/30 backdrop-blur-xl border border-border-light dark:border-border-dark z-[100]',
-          context.isOpen ? 'animate-fadeIn' : 'animate-fadeOut'
+          'absolute mt-2 w-fit rounded-md flex flex-col gap-2 p-3 bg-gray-100/30 dark:bg-dark-400/30 backdrop-blur-xl border border-border-light dark:border-border-dark z-[100]',
+          {
+            left: 'left-0',
+            right: 'right-0',
+          }[position],
+          context.isOpen ? 'animate-fadeIn' : 'animate-fadeOut',
+          className
         )}
       >
         {children}
