a7afe599583b7f9e1cd6caee692a46da576cd186
Anton Kolot <antvelkol@gmail.com>
2025-02-09 16:16:41 +0100

    Update user store to use Network and WalletType enums

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,15 +1,18 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
+import { Network } from '../config/chains';
+import { WalletType } from '../config/wallets';
 
 /**
  * @interface UserState
  * @description Interface defining the shape of the user's state.
  */
 interface UserState {
-  connectedNetwork: 'ethereum' | 'tron' | null; // Currently connected network
+  connectedNetwork: Network | null; // Currently connected network
   address: string | null; // Wallet address of the connected user
+  wallet: WalletType | null; // Connected wallet type
   isConnected: boolean; // Connection status flag
-  setConnectedNetwork: (network: 'ethereum' | 'tron', address: string) => void; // Function to set the connected network and address
+  setConnectedNetwork: (network: Network, address: string, wallet: WalletType) => void; // Function to set the connected network and address
   resetConnection: () => void; // Function to reset the connection state
 }
 
@@ -22,30 +25,37 @@ export const useUserStore = create(
     (set) => ({
       connectedNetwork: null,
       address: null,
+      wallet: null,
       isConnected: false,
+
       /**
        * @function setConnectedNetwork
        * @description Sets the connected network and address in the store.
-       * @param {string} network - The network to connect to ('ethereum' or 'tron').
+       * @param {Network} network - The network to connect to.
        * @param {string} address - The wallet address of the connected user.
        */
-      setConnectedNetwork: (network, address) => {
-        console.log('setConnectedNetwork', network, address);
+      setConnectedNetwork: (network, address, wallet) => {
+        if (!Object.values(Network).includes(network)) {
+          console.error(`Invalid network: ${network}`);
+          return;
+        }
         set({
           connectedNetwork: network,
           address,
+          wallet,
           isConnected: true,
         });
       },
+
       /**
        * @function resetConnection
        * @description Resets the wallet connection state and clears any persistent storage related to the connection.
        */
       resetConnection: () => {
-        console.log('resetConnection');
         set({
           connectedNetwork: null,
           address: null,
+          wallet: null,
           isConnected: false,
         });
       },
