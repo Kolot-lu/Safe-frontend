e2b09e6e76fc9a31997719b680906ad40ac13ec4
Anton Kolot <antvelkol@gmail.com>
2025-02-20 18:23:04 +0100

    Update token addresses for EthereumSepolia network

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -22,10 +22,10 @@ export const initTokensList: Record<Network, Token[]> = {
   [Network.EthereumMainnet]: [{ name: 'ETH', address: ZERO_ADDRESS }],
   [Network.EthereumSepolia]: [
     { name: 'ETH', address: ZERO_ADDRESS },
-    { name: 'USDT', address: '0x7169D38820dfd117C3FA1f22a697dBA58d90BA06' },
-    { name: 'WBTC', address: '0xe474b1939D11E17325B9A698462D89D3c47186F9' },
-    { name: 'DAI', address: '0x68194a729C2450ad26072b3D33ADaCbcef39D574' },
-    { name: 'USDC', address: '0x097Da51357837f9A0760B04cae4c23c3ebE28B00' },
+    { name: 'USDT', address: '0x419Fe9f14Ff3aA22e46ff1d03a73EdF3b70A62ED'},
+    { name: 'OKB', address: '0x3F4B6664338F23d2397c953f2AB4Ce8031663f80' },
+    { name: 'DAI', address: '0xf5c142292B85253e4D071812c84f05ec42828fdB' },
+    { name: 'USDC', address: '0x13fA158A117b93C27c55b8216806294a0aE88b6D' },
   ],
   [Network.TronMainnet]: [{ name: 'TRX', address: ZERO_ADDRESS }],
 };
